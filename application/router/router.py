68c24fbcbaa20feb308fab4ba6ab08164d08798f
chisanucha-plum <chisanucha.plum@gmail.com>
2025-06-19 13:30:30 +0700

    fix code

---
+++ application/router/router.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from application.controller.controller import (
+    helmet_detection_stream,
+)
+
+router = APIRouter()
+
+
+generate_router = APIRouter(prefix="/detection")
+generate_router.get("/helmet")(helmet_detection_stream)
+router.include_router(generate_router)
