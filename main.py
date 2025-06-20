e89831d241b30a4978a12c035c6d2c93810b1a98
chisanucha-plum <chisanucha.plum@gmail.com>
2025-06-20 10:24:09 +0700

    fix import path for router module

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from application.router.router import router
+from app.router.router import router
 
 app = FastAPI()
 
