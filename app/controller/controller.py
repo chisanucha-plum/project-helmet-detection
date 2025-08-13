0899809f87d76f2ed538490568930e02f223d47c
chisanucha-plum <chisanucha.plum@gmail.com>
2025-08-13 16:06:39 +0700

    refactor: improve code formatting and readability in controller and helmet model

--- app/controller/controller.py
+++ app/controller/controller.py
@@ -1,23 +1,28 @@
 import logging
-import os
+
 import cv2
 from fastapi.responses import StreamingResponse
+
 from app.configuration import *
+from app.configuration import Configuration
 from app.model.helmet import ObjectDetect
 from app.service.visualizer import DetectionVisualizer
-from app.configuration import Configuration
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 async def generate_frames():
     config = Configuration.get_config()
     detector = ObjectDetect(
-        config.model_settings.helmet_model_path,
-        config.model_settings.person_model_path
+        config.model_settings.helmet_model_path, config.model_settings.person_model_path
     )
     visualizer = DetectionVisualizer()
-    cap = cv2.VideoCapture(config.application_settings.webcam_id if config.application_settings.use_webcam else config.application_settings.video_path)
+    cap = cv2.VideoCapture(
+        config.application_settings.webcam_id
+        if config.application_settings.use_webcam
+        else config.application_settings.video_path
+    )
     if not cap.isOpened():
         logger.error("Cannot open video source")
         raise ValueError("Cannot open video source")
@@ -26,17 +31,24 @@ async def generate_frames():
         if not ret:
             break
         results_helmet, results_person = detector.detect(
-            frame, config.model_settings.helmet_conf_threshold, config.model_settings.person_conf_threshold
+            frame,
+            config.model_settings.helmet_conf_threshold,
+            config.model_settings.person_conf_threshold,
         )
         frame, has_no_helmet = visualizer.draw_detections(
-            frame, results_helmet, results_person, config.detection_visualizer.roi_points
+            frame,
+            results_helmet,
+            results_person,
+            config.detection_visualizer.roi_points,
         )
         # encode frame as jpeg
-        ret, buffer = cv2.imencode('.jpg', frame)
+        ret, buffer = cv2.imencode(".jpg", frame)
         frame_bytes = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        yield (b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame_bytes + b"\r\n")
     cap.release()
 
+
 async def helmet_detection_stream():
-    return StreamingResponse(generate_frames(), media_type='multipart/x-mixed-replace; boundary=frame')
+    return StreamingResponse(
+        generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame"
+    )
